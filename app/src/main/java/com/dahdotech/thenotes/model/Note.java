042588448ca6099a7f51a7d59bd17dcde400a8ab
Dahdo <dndashimye15@gmail.com>
2022-08-21 17:04:43 +0200

    add Note model class

---
+++ app/src/main/java/com/dahdotech/thenotes/model/Note.java
@@ -0,0 +1,66 @@
+package com.dahdotech.thenotes.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "note_table")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title;
+    private String content;
+    private Date time;
+
+    public Note(){
+
+    }
+
+    public Note(String title, String content, Date time) {
+        this.title = title;
+        this.content = content;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
